d187b8df706cef7aa58889b46adcf695a82f7064
Hicabi Kara <hicabi_kara01@hotmail.com>
2024-08-30 14:48:42 +0300

    app içersindeki dosya yapısı (main) ve (auth) olarak güncellendi giriş yapma ve üye olma alanları root layouttan ayrıştırıldı)

---
+++ src/app/(main)/aboutus/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./aboutus.module.css";
+const Hakkımızda = () => {
+  return (
+    <main className={styles.container}>
+      <section className={styles.sectionContainer}>
+        Hakkımızda
+      </section>
+    </main>
+  );
+};
+
+export default Hakkımızda;
