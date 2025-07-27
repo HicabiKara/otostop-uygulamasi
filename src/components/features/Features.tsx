8630ff3776879bf8a02af45dfb95051f01800a4c
Hicabi Kara <hicabi_kara01@hotmail.com>
2025-07-27 14:11:13 +0300

    G sap ile ilgili uygunsuz kullanımlar düzeltildi ve kod refactoring yapıldı.

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -1,131 +1,103 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import styles from "./features.module.css";
 import Image from "next/image";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const Features = () => {
-  useGSAP(()=>{
-    gsap.to("#content",{
-      scrollTrigger:{
-        trigger:"#content",
-        start:"top 90%"
+  const column1Ref = useRef(null);
+  const column2Ref = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(column1Ref.current, {
+      scrollTrigger: {
+        trigger: column1Ref.current,
+        start: "top 90%",
       },
-      opacity:1,
-      y:"0",
-      delay:1.5,
-      duration:1
-    })
-  })
+      opacity: 1,
+      y: 0,
+      delay: 0.3,
+      duration: 1,
+    });
+
+    gsap.to(column2Ref.current, {
+      scrollTrigger: {
+        trigger: column2Ref.current,
+        start: "top 90%",
+      },
+      opacity: 1,
+      y: 0,
+      delay: 0.6,
+      duration: 1,
+    });
+  }, []);
+
   return (
     <section className={styles.container}>
-      <div id="content" className={styles.column}>
+      <div ref={column1Ref} className={styles.column}>
         <h1>Yolculuğa mı çıkmak istiyorsunuz?</h1>
-        <div className={styles.content}>
-          <div className={styles.imageContainer}>
-            <Image
-              src="/feature-img1.jpg"
-              width={250}
-              height={250}
-              alt="feature-img"
-            />
-          </div>
-          <div className={styles.contentContainer}>
-            <h2>Üye olun ve giriş yapın.</h2>
-            <p>
-              İlk olarak üyelik işlemlerinizi gerçekleştirin.Ardından giriş
-              yaparak ilk adımı tamamlayın
-            </p>
-          </div>
-        </div>
-        <div className={styles.content}>
-          <div className={styles.imageContainer}>
-            <Image
-              src="/feature-img2.jpg"
-              width={250}
-              height={250}
-              alt="feature-img"
-            />
-          </div>
-          <div className={styles.contentContainer}>
-            <h2>Kendinize uygun ilanı bulun ve rezervasyon isteği gönderin.</h2>
-            <p>
-            Aktif ilanlar sayfasından kendinize uygun yolculuk ilanını bulun.Ardından bu ilana rezervasyon talebinde bulunun.
-            </p>
-          </div>
-        </div>
-        <div className={styles.content}>
-          <div className={styles.imageContainer}>
-            <Image
-              src="/feature-img3.jpg"
-              width={250}
-              height={250}
-              alt="feature-img"
-            />
-          </div>
-          <div className={styles.contentContainer}>
-            <h2>Yolculuğun tadını çıkarın.</h2>
-            <p>
-              İlan sahibi talebinizi onayladıktan sonra iglili ilan tarihi ve saatinde yolculuğunuza başlayabilirsiniz.İyi yolculuklar
-            </p>
-          </div>
-        </div>
+
+        <FeatureContent
+          img="/feature-img1.jpg"
+          title="Üye olun ve giriş yapın."
+          desc="İlk olarak üyelik işlemlerinizi gerçekleştirin. Ardından giriş yaparak ilk adımı tamamlayın."
+        />
+        <FeatureContent
+          img="/feature-img2.jpg"
+          title="Kendinize uygun ilanı bulun ve rezervasyon isteği gönderin."
+          desc="Aktif ilanlar sayfasından kendinize uygun yolculuk ilanını bulun. Ardından bu ilana rezervasyon talebinde bulunun."
+        />
+        <FeatureContent
+          img="/feature-img3.jpg"
+          title="Yolculuğun tadını çıkarın."
+          desc="İlan sahibi talebinizi onayladıktan sonra ilgili ilan tarihi ve saatinde yolculuğunuza başlayabilirsiniz. İyi yolculuklar."
+        />
       </div>
 
-      <div id="content" className={styles.column}>
+      <div ref={column2Ref} className={styles.column}>
         <h1>Yol arkadaşı mı arıyorsunuz?</h1>
-        <div className={styles.content}>
-          <div className={styles.imageContainer}>
-            <Image
-              src="/feature-img1.jpg"
-              width={250}
-              height={250}
-              alt="feature-img"
-            />
-          </div>
-          <div className={styles.contentContainer}>
-            <h2>Üye olun ve giriş yapın</h2>
-            <p>
-              İlk olarak üyelik işlemlerinizi gerçekleştirin.Ardından giriş
-              yaparak ilk adımı tamamlayın
-            </p>
-          </div>
-        </div>
-        <div className={styles.content}>
-          <div className={styles.imageContainer}>
-            <Image
-              src="/feature-img2.jpg"
-              width={250}
-              height={250}
-              alt="feature-img"
-            />
-          </div>
-          <div className={styles.contentContainer}>
-            <h2>Yolculuk ilanınızı hızlı ve kolay bir şekilde oluşturun.</h2>
-            <p>
-                Yolculuk yayınla alanından kendinize uygun ilanı oluşturarak ilanınızı yayınlayın.
-            </p>
-          </div>
-        </div>
-        <div className={styles.content}>
-          <div className={styles.imageContainer}>
-            <Image
-              src="/feature-img3.jpg"
-              width={250}
-              height={250}
-              alt="feature-img"
-            />
-          </div>
-          <div className={styles.contentContainer}>
-            <h2>Yolculuğun tadını çıkarın.</h2>
-            <p>
-            İlgili ilanınız için gelen yolculuk taleplerini inceleyerk uygun talebi veya talepleri onaylayın.Yolcularınızla buluşup yolculuğunuza başlayabilirsiniz.İyi yolculuklar
-            </p>
-          </div>
-        </div>
+        <FeatureContent
+          img="/feature-img1.jpg"
+          title="Üye olun ve giriş yapın"
+          desc="İlk olarak üyelik işlemlerinizi gerçekleştirin. Ardından giriş yaparak ilk adımı tamamlayın."
+        />
+        <FeatureContent
+          img="/feature-img2.jpg"
+          title="Yolculuk ilanınızı hızlı ve kolay bir şekilde oluşturun."
+          desc="Yolculuk yayınla alanından kendinize uygun ilanı oluşturarak ilanınızı yayınlayın."
+        />
+        <FeatureContent
+          img="/feature-img3.jpg"
+          title="Yolculuğun tadını çıkarın."
+          desc="İlanınıza gelen talepleri inceleyerek uygun talebi onaylayın. Yolcularla buluşup yolculuğunuza başlayın. İyi yolculuklar."
+        />
       </div>
     </section>
   );
 };
 
 export default Features;
+
+const FeatureContent = ({
+  img,
+  title,
+  desc,
+}: {
+  img: string;
+  title: string;
+  desc: string;
+}) => (
+  <div className={styles.content}>
+    <div className={styles.imageContainer}>
+      <Image src={img} width={250} height={250} alt="feature-img" />
+    </div>
+    <div className={styles.contentContainer}>
+      <h2>{title}</h2>
+      <p>{desc}</p>
+    </div>
+  </div>
+);
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,31 +1,35 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import styles from "./hero.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+
 const Hero = () => {
-  useGSAP(()=>{
-   gsap.to("#heroTitle",{opacity:1,y:"-50",delay:0.3}) 
-   gsap.to("#startLink",{opacity:1,y:"-40",delay:0.4})
-  gsap.to("#rightContent",{opacity:1,x:"0",delay:0.3})
-  })
+  const heroTitleRef = useRef(null);
+  const startLinkRef = useRef(null);
+  const rightContentRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(heroTitleRef.current, { opacity: 1, y: -50, delay: 0.3 });
+    gsap.to(startLinkRef.current, { opacity: 1, y: -40, delay: 0.4 });
+    gsap.to(rightContentRef.current, { opacity: 1, x: 0, delay: 0.3 });
+  }, []);
 
   return (
     <section className={styles.container}>
       <div className={styles.leftBar}>
-        <div id="heroTitle"
-         className={styles.leftContent}>
+        <div ref={heroTitleRef} className={styles.leftContent}>
           <h1>Düşük Ücretler İle Kesintisiz Seyehat Edin</h1>
         </div>
-        <div id="startLink" className={styles.linkContainer}>
+        <div ref={startLinkRef} className={styles.linkContainer}>
           <Link href="/signin" className={styles.link}>
             Hemen Başla
           </Link>
         </div>
       </div>
-      <div id="rightContent" className={styles.rightBar}>
+      <div ref={rightContentRef} className={styles.rightBar}>
         <Image
           className={styles.image}
           src="/hero.jpg"
